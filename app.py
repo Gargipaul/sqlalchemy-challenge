f57ed48558a636021d9e062cb090e2087ceb3848
Gargipaul <53881008+Gargipaul@users.noreply.github.com>
2019-10-24 12:06:13 -0400

    check

--- app.py
+++ app.py
@@ -8,40 +8,50 @@ from sqlalchemy import create_engine, func, desc
 
 from flask import Flask, jsonify
 
-
-engine = create_engine(r"sqlite:///C:\Users\gargi\sqlalchemy-challenge\Resources\hawaii.sqlite")
+##############
+engine = create_engine(r'sqlite:///C:\Users\gargi\sqlalchemy-challenge\Instructions\Resources\hawaii.sqlite')
 
 
 Base = automap_base()
+
+
 Base.prepare(engine, reflect=True)
-measurement = Base.classes.measurement
-station = Base.classes.stations
+
+
+Measurement = Base.classes.measurement
+
+Station = Base.classes.station
+
+
 session = Session(engine)
-
-
 
 
 # Flask Setup
 #################
 app = Flask(__name__)
 
+@app.route("/")
+def welcome():
+    
+    return ("Welcome to Hawaii")
+    
 
 
-# Flask Routes
-##################
+
+
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
    
    session = Session(engine)
    
-   
-   prcp_data= session.query(measurement.date, measurement.prcp).all()
 
+   prcp_data= session.query(Measurement.date, Measurement.prcp).all()
+   print(prcp_data)
    session.close()
    
-
    all_data = []
+
    for date, prcp in prcp_data:
        prcp_dict = {}
        prcp_dict["date"] = date
@@ -50,36 +60,27 @@ def precipitation():
    return jsonify(all_data)
 
 
+@app.route("/api/v1.0/precipitation")
+def precip():
 
 
     
-
-@app.route("/api/v1.0/tobs")
-def tobs():
-   
-   session = Session(engine)
-   
-   
-   tobs_data= session.query(measurement.date, measurement.tobs).all()
-
-   session.close()
-   
-
-   tobs_data = []
-   results = session.query(measurement).filter(measurement.date > '2016-10-09').filter(measurement.date <= '2017-10-09').all()
-   for data in results:
+    all_tobs = []
+    results = session.query(measurement).filter(measurement.date > '2016-10-09').filter(measurement.date <= '2017-10-09').all()
+    for data in results:
         tobs_dict = {}
         tobs_dict[data.date] = data.tobs
-        tobs_data.append(tobs_dict)
-   return jsonify(all_tobs)
+        all_tobs.append(tobs_dict)
+
+    return jsonify(all_tobs)
 
 
 @app.route("/api/v1.0/stations")
 def stations():
     
 
-    
-    station_results = session.query(station.station).all()
+   
+    station_results = session.query(Station.station).all()
 
     
     station_list = list(np.ravel(station_results))
@@ -94,7 +95,7 @@ def tobs():
     
     tobs_results = session.query(measurement.tobs).all()
 
-    
+  
     tobs_list = list(np.ravel(tobs_results))
 
     return jsonify(tobs_list)
